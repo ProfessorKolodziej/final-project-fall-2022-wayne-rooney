f51fbc8af62adcf8a788d668addadc690412ba8d
guofengzhang1998 <zzggff135@gmail.com>
2023-12-05 22:49:18 -0500

    delete not functioning script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById("myVideo");
     var playButton = document.getElementById("playbutton");
     var fadeButton = document.getElementById("clickbutton");
-    var sections = document.querySelectorAll(".scrollable-section");
 
     playButton.addEventListener("click", function() {
       if (video.paused) {
